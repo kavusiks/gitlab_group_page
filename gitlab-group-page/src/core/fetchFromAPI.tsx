f16a8c0649a2f41651f15ba73bc3d5c6d613b9ea
francinv <francinv@stud.ntnu.no>
2021-09-24 13:36:36 +0200

    (#2) Added button to fetch API

--- gitlab-group-page/src/core/fetchFromAPI.tsx
+++ gitlab-group-page/src/core/fetchFromAPI.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-<<<<<<< HEAD
 import { fetchAPI } from './APIfunction';
 export class FetchFromAPI extends React.Component {
-=======
 
-export class fetchFromAPI extends React.Component {
->>>>>>> abbfb55 ((#2) Wrote component to fetch data from API)
     state = {
         url: 'https://gitlab.stud.idi.ntnu.no/api/v4/projects/11911/',
         token: 'GSK4CqSZCdZGPz4NsAHw',
         loading: true,
-<<<<<<< HEAD
         payload: {
                     'test': 1}
         }
-=======
-        payload: []
-    }
-    
->>>>>>> abbfb55 ((#2) Wrote component to fetch data from API)
 
     /*
     returnStatus(response) {
@@ -37,17 +27,13 @@ export class fetchFromAPI extends React.Component {
                         'PRIVATE-TOKEN': this.state.token
                     }
                 }) // use state or props
-<<<<<<< HEAD
         let data = await response.json();
         console.log(data)
-=======
-        let data = await response.json()
->>>>>>> abbfb55 ((#2) Wrote component to fetch data from API)
         this.setState({
             loading: false,
             payload: data
         })
-<<<<<<< HEAD
+
         console.log(this.state.payload);
         const getElements = async () => {
             const test = await fetchAPI(11911, 'GSK4CqSZCdZGPz4NsAHw')
@@ -55,43 +41,21 @@ export class fetchFromAPI extends React.Component {
         }
         getElements();
 
-=======
->>>>>>> abbfb55 ((#2) Wrote component to fetch data from API)
+        console.log(this.state.payload);
         //let data = await returnStatus(response.json())
         // .catch(error)
     }
 
     render() {
         const { loading, payload } = this.state;
-<<<<<<< HEAD
         console.log(payload);
         console.log(typeof payload);
         const testList = [1, 2, 3, 4];
         const listItems = testList.map((item) => {
             return <li>{ item }</li>
         });
-=======
->>>>>>> abbfb55 ((#2) Wrote component to fetch data from API)
 		return (
 			<div>
 				{loading && <div>Loading...</div>}
-                {!loading &&
-                    <div >
-<<<<<<< HEAD
-                        <ul>
-                            {listItems}
-                        </ul>
-                    </div>
-                }
-                <div>
-                    <button onClick={this.getRepository}> Press me </button>
-                </div>
-=======
-                        {payload[5]}
-                    </div>
-                }
->>>>>>> abbfb55 ((#2) Wrote component to fetch data from API)
 			</div>
 		);
-	}
-}

d4a43ea64c9f9f063e3141a55d1a7b6860beedff
Dherik <dherikjd@stud.ntnu.no>
2021-09-28 10:48:05 +0200

    (#2) Added functional API calls

--- gitlab-group-page/src/core/fetchFromAPI.tsx
+++ gitlab-group-page/src/core/fetchFromAPI.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import { fetchAPI } from './APIfunction';
 export class FetchFromAPI extends React.Component {
     state = {
         url: 'https://gitlab.stud.idi.ntnu.no/api/v4/projects/11911/',
         token: 'GSK4CqSZCdZGPz4NsAHw',
         loading: true,
-        payload: []
-    }
-    
+        payload: {
+                    'test': 1}
+        }
 
     /*
     returnStatus(response) {
@@ -26,31 +26,45 @@ export class FetchFromAPI extends React.Component {
                         'PRIVATE-TOKEN': this.state.token
                     }
                 }) // use state or props
-        let data = await response.json()
+        let data = await response.json();
+        console.log(data)
         this.setState({
             loading: false,
             payload: data
         })
         console.log(this.state.payload);
+        const getElements = async () => {
+            const test = await fetchAPI(11911, 'GSK4CqSZCdZGPz4NsAHw')
+            console.log(test)
+        }
+        getElements();
+
         //let data = await returnStatus(response.json())
         // .catch(error)
     }
 
     render() {
         const { loading, payload } = this.state;
+        console.log(payload);
+        console.log(typeof payload);
+        const testList = [1, 2, 3, 4];
+        const listItems = testList.map((item) => {
+            return <li>{ item }</li>
+        });
 		return (
 			<div>
 				{loading && <div>Loading...</div>}
                 {!loading &&
                     <div >
-                        {payload[5]}
+                        <ul>
+                            {listItems}
+                        </ul>
                     </div>
                 }
                 <div>
-                    <button onClick={this.getRepository}>Press me </button>
+                    <button onClick={this.getRepository}> Press me </button>
                 </div>
 			</div>
-            
 		);
 	}
 }

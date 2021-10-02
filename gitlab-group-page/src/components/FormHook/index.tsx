ce4b83f0f922fe7939d423708c482993306dee18
francinv <francinv@stud.ntnu.no>
2021-10-02 22:38:28 +0200

    (#9) Routing for Login

--- gitlab-group-page/src/components/FormHook/index.tsx
+++ gitlab-group-page/src/components/FormHook/index.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 export const useForm = (callback: any, initialState = {}) => {
     const [values, setValues] = useState(initialState);
+    const history = useHistory();
 
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setValues({...values, [event.target.name]: event.target.value});
@@ -10,6 +12,7 @@ export const useForm = (callback: any, initialState = {}) => {
     const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         await callback();
+        history.push("/main-page");
     }
 
     return {

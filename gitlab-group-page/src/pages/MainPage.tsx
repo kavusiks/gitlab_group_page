b3dde27539b648db4f7fc90ff54523a1c74e755d
francinv <francinv@stud.ntnu.no>
2021-10-02 22:39:11 +0200

    (#9) Added routing if user not logged in

--- gitlab-group-page/src/pages/MainPage.tsx
+++ gitlab-group-page/src/pages/MainPage.tsx
@@ -1,9 +1,38 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect } from "react";
 
 import { Row, Col } from "react-bootstrap";
+import { useHistory } from "react-router";
 import NavigationBar from "../components/NavigationBar";
+import { fetchProject } from "../core/APIfunction";
 
 export const MainPage: FunctionComponent = () => {
+  
+  const history = useHistory();
+  
+  useEffect(() => {
+    if ((localStorage.getItem("Group ID")===null) && (localStorage.getItem("Group Access Token")===null) ) {
+      history.push('/');
+    } else {
+      checkAPI();
+      history.push('/main-page');
+    }
+  });
+
+  async function checkAPI() {
+    const temp = await fetchProject(
+      JSON.parse(localStorage.getItem('Group ID') || '{}'),
+      JSON.parse(localStorage.getItem('Group Access Token') || '{}')
+    );
+
+    if (temp.message === "401 Unauthorized") {
+      history.push('/');
+    }
+    else if (temp.message === "404 Project Not Found") {
+      history.push('/');
+    }
+
+  }
+
   return (
     <>
       <NavigationBar></NavigationBar>

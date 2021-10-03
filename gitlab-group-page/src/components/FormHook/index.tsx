f027152eed83dfab07ed016e711ed867e4f54be7
francinv <francinv@stud.ntnu.no>
2021-10-03 23:39:16 +0200

    (#13) Comments on FormHook

--- gitlab-group-page/src/components/FormHook/index.tsx
+++ gitlab-group-page/src/components/FormHook/index.tsx
@@ -7,18 +7,22 @@ export const useForm = (callback: any, initialState = {}) => {
   const [values, setValues] = useState(initialState);
   const history = useHistory();
 
+  /**
+   * This method will set the values when something in the input fields are changed. 
+   */
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValues({ ...values, [event.target.name]: event.target.value });
   };
 
+  /**
+   * Method that handles submit. When the user submits the form. It will call the loginuUserCallback() 
+   * and also change page. 
+   */
   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     await callback();
-    history.push('/labels')
+    history.push("/labels");
   };
-
-
-
 
   return {
     onChange,

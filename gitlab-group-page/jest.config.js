7b2bc04d40037c2ee5e5333ab04e0625a1d632fe
D <dherikjd@stud.ntnu.no>
2021-10-03 22:39:39 +0200

    (#14) Refactoring

--- gitlab-group-page/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "react-native",
-  moduleFileExtension: ["ts", "tsx", "js", "jsx", "json", "node"],
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.test.json'
-    }
-  }
-};

9ba02590d153c97fd4efcad28544571ca059201b
D <dherikjd@stud.ntnu.no>
2021-10-03 23:23:53 +0200

    (#13) Commented some code

--- gitlab-group-page/src/App.test.tsx
+++ gitlab-group-page/src/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import App from "./App";
 import TestRenderer from 'react-test-renderer';
 
+// Testing snapshot-test. Just creates a snapshot if there is none saved from before. If snapshot of rendered component is equal to saved snapshot, test passes.
 
 it('renders correctly', () => {
   const tree = TestRenderer.create(<App />).toJSON();

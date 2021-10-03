b2ab22c5969c120d22eea864e57ef3c091f4b297
kavusi98 <kavusikan1998@gmail.com>
2021-10-03 23:46:33 +0200

    (#13) Added docs for api functions

--- gitlab-group-page/src/core/APIfunction.ts
+++ gitlab-group-page/src/core/APIfunction.ts
@@ -1,17 +1,28 @@
+/**
+ *
+ * @param id groupId
+ * @param token accessToken
+ * @returns the JSON response
+ */
 export const fetchProject = async (id: number, token: string) => {
   let url = `https://gitlab.stud.idi.ntnu.no/api/v4/projects/${id}`;
   const response = await (
     await fetch(url, {
-      // use state or props
       headers: {
         "Content-Type": "application/json",
         "PRIVATE-TOKEN": token,
       },
     })
   ).json();
-  return response; //TODO: reduce
+  return response;
 };
 
+/**
+ *
+ * @param id groupId
+ * @param token accessToken
+ * @returns Stringifies JSON-object
+ */
 export const fetchLabels = async (id: number, token: string) => {
   let url = `https://gitlab.stud.idi.ntnu.no/api/v4/projects/${id}/labels`;
   const response = await (
@@ -27,45 +38,22 @@ export const fetchLabels = async (id: number, token: string) => {
   return JSON.stringify(response);
 };
 
-export const fetchCommits = async (id: number, token: string) => {
-  let url = `https://gitlab.stud.idi.ntnu.no/api/v4/projects/${id}/repository/commits`;
-  const response = await (
-    await fetch(url, {
-      // use state or props
-      headers: {
-        "Content-Type": "application/json",
-        "PRIVATE-TOKEN": token,
-      },
-    })
-  ).json();
-  return JSON.stringify(response);
-};
-
+/**
+ *
+ * @param id groupId
+ * @param token accessToken
+ * @returns Stringifies JSON-object
+ */
 export const fetchIssues = async (id: number, token: string) => {
   let url = `https://gitlab.stud.idi.ntnu.no/api/v4/projects/${id}/issues`;
   const response = await (
     await fetch(url, {
-      // use state or props
       headers: {
         "Content-Type": "application/json",
         "PRIVATE-TOKEN": token,
       },
     })
   ).json();
-  //return response;
-  return JSON.stringify(response);
-};
 
-export const fetchHistory = async (id: number, token: string) => {
-  let url = `https://gitlab.stud.idi.ntnu.no/api/v4/projects/${id}/events`;
-  const response = await (
-    await fetch(url, {
-      // use state or props
-      headers: {
-        "Content-Type": "application/json",
-        "PRIVATE-TOKEN": token,
-      },
-    })
-  ).json();
   return JSON.stringify(response);
 };
